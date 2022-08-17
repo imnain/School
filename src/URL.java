39b5b7fd69e60aebb91e7cf07078d0b03bd341f2
mandeep <mandeepnain84@gmail.com>
2022-08-17 19:02:42 +0530

    First commit of students program

---
+++ src/URL.java
@@ -0,0 +1,6 @@
+public class URL {
+    public static String url = "jdbc:mysql://localhost:3306/students";
+    public static String user = "mandeepnain";
+    public static String password = "Qwertyui@123";
+    public static String url1 = "com.mysql.cj.jdbc.Driver";
+}
